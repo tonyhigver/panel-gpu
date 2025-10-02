07f72b20fb62d78200acfd0af836fbddb0342837
tonyhigver <tonyhigver@gmail.com>
2025-10-02 17:27:55 +0200

    Integrada barra lateral en SaaSMain con enlaces a Miniaturas, CTR

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      isAdmin: boolean;
+      hasPaid: boolean;
+    } & DefaultSession["user"];
+  }
+  
+  interface User {
+    id: string;
+    isAdmin: boolean;
+    hasPaid: boolean;
+  }
+}
